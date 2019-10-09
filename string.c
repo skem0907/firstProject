f6ecb68257d89ef36579dd94cb3d25207cbfc074
skem0907 <skem0907@skl.com.tw>
2019-10-09 13:45:42 +0800

    Added my_strcpy function

--- string.c
+++ string.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-int my_strlen(char *s)
+size_t my_strlen(char *s)
 {
 	char *p = s;
 	while (*p) ++p;
 	return (p - s);
 }
-
+char *my_strcpy(char *t, char *s)
+{
+	char *p = t; 
+	while (*t ++ = *s ++);
+	return p;
+}
 int main(void) {
 	int i;
-
+        char p1[32];    
 	char *s[] = {
 		"Git tutorials", "Hello World"
 	};
